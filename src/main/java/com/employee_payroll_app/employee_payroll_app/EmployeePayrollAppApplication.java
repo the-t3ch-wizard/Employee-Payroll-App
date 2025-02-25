e3ffd8bc0ba62859e3de2b0ab8ad616404c0deda
ayush <ayushmaurya2050@gmail.com>
2025-02-25 11:52:32 +0530

    [AYUSH KUMAR MAURYA] Add: setup project

---
+++ src/main/java/com/employee_payroll_app/employee_payroll_app/EmployeePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.employee_payroll_app.employee_payroll_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee_payroll_app/employee_payroll_app/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_payroll_app.employee_payroll_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
